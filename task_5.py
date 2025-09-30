d3fd8d528d9445054774c2a73fc3ab9cf7a75e75
Egor <egor0908@gmail.com>
2025-09-30 12:10:05 +0300

    Повторное исправление ошибок
    В task_2 убрал код в комментариях
    В task_5 заменил pop() на del

--- task_5.py
+++ task_5.py
@@ -21,7 +21,8 @@ class TestCase:
     def set_step(self, step_number, step_text):
         self.steps[step_number] = step_text
     def delete_step(self, step_number):
-        self.steps.pop(step_number)
+        if step_number in self.steps:
+            del self.steps[step_number]
     def set_result(self, result):
         self.result = result
     def get_test_case(self):
